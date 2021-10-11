3a706635848ec90f72b5b505293e20b1e5477f3d
unknown <hackerbpm30@gmail.com>
2021-10-11 16:51:06 +0530

    netlify issue

--- app.js
+++ app.js
@@ -11,12 +11,8 @@ const dotenv = require('dotenv').config()
 
 const app = express();
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 
 require("./config/strategy")(passport);
